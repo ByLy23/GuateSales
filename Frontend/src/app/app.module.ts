3e8a8d01301f564a06b1c62e3c1891fb6d8b394a
3018080310101@ingenieria.usac.edu.gt <3018080310101@ingenieria.usac.edu.gt>
2020-10-17 01:33:57 -0600

    inicio sesion

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    InicioSesionComponent
   ],
   imports: [
     BrowserModule,
